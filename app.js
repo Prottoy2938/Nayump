4b6160783885643fb81db65e3348478cac8f7522
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-03 16:56:43 +0600

    Created folder structure

--- app.js
+++ app.js
@@ -1,41 +1,8 @@
-// const text = start.querySelector(".h1");
-// const video = start.querySelector("video");
-// const end = document.querySelector(".end");
-
-// var controller = new ScrollMagic.Controller();
-
-// const scene = new ScrollMagic.Scene({
-//   triggerElement: h1,
-//   duration: 1000,
-//   triggerHook: 0
-// })
-//   .setPin(h1)
-//   .addTo(controller)
-//   .addIndicators();
-
-// // TweenLite.delayedCall(3, myFunction);
-
-// // function myFunction() {
-// //   TweenLite.to(text, 3, { left: 600, color: "#8f3d38", opacity: 0.5 });
-// // }
-// // TweenLite.to(text, 3, { left: 600, color: "#8f3d38", opacity: 0.5 });
-
-// let accelamount = 0.1;
-// let scrollpos = 0;
-// let delay = 0;
-
-// scene.on("update", e => {
-//   scrollpos = e.scrollPos / 800;
-//   //   console.log(e);
-// });
-
-// setInterval(() => {
-//   delay += (scrollpos - delay) * accelamount;
-//   video.currentTime = delay;
-//   // console.log(video.currentTime, scrollpos);
-// }, 40);
-
-// This code is from another Codepen
+console.log("Video from Pexel");
+console.log(
+  "Beautiful Flower favicon icon by Icons8",
+  "https://icons8.com/icons/set/beautiful-flower"
+);
 
 var frameNumber = 0; // start video at frame 0
 // lower numbers = faster playback
